101aac34013ea0e40aeaf752172c9107fa3741ff
jonathancxtt <choojonathanxitong@gmail.com>
2026-09-21 23:17:28 +0800

    Added aws api gateway form integration

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -7,3 +7,28 @@ resource "aws_api_gateway_resource" "form_endpoint" {
   path_part = "submit"
   rest_api_id = aws_api_gateway_rest_api.form_api.id
 }
+
+resource "aws_api_gateway_method" "form_method" {
+  rest_api_id   = aws_api_gateway_rest_api.form_api.id
+  resource_id   = aws_api_gateway_resource.form_endpoint.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "form_integration" {
+  rest_api_id = aws_api_gateway_rest_api.form_api.id
+  resource_id = aws_api_gateway_resource.form_endpoint.id
+  http_method = "POST"
+  type = "AWS_PROXY"
+
+  integration_http_method = "POST"
+  uri = aws_lambda_function.form_handler.invoke_arn
+}
+
+//Options Configuration to handle CORS integration
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id = aws_api_gateway_rest_api.form_api.id
+  resource_id = aws_api_gateway_resource.form_endpoint.id
+  http_method = "OPTIONS"
+  authorization = "NONE"
+}
