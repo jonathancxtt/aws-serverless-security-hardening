2bc3de813d219842e909377e42d31f0a251c821a
jonathancxtt <choojonathanxitong@gmail.com>
2026-09-23 23:11:59 +0800

    Add API Gateway with POST and OPTIONS methods, Lambda integration, and stage

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -26,9 +26,80 @@ resource "aws_api_gateway_integration" "form_integration" {
 }
 
 //Options Configuration to handle CORS integration
-resource "aws_api_gateway_method" "options_method" {
+resource "aws_api_gateway_method" "cors_options_method" {
   rest_api_id = aws_api_gateway_rest_api.form_api.id
   resource_id = aws_api_gateway_resource.form_endpoint.id
   http_method = "OPTIONS"
   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "cors_integration" {
+  rest_api_id = aws_api_gateway_rest_api.form_api.id
+  resource_id = aws_api_gateway_resource.form_endpoint.id
+  http_method = aws_api_gateway_method.cors_options_method.http_method
+  type = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "cors_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.form_api.id
+  resource_id = aws_api_gateway_resource.form_endpoint.id
+  http_method = aws_api_gateway_method.cors_options_method.http_method
+  status_code = "200"
+
+  depends_on = [aws_api_gateway_integration.cors_integration]
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'"
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
+resource "aws_api_gateway_method_response" "cors_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.form_api.id
+  resource_id = aws_api_gateway_resource.form_endpoint.id
+  http_method = aws_api_gateway_method.cors_options_method.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_deployment" "form_api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.form_api.id
+
+  depends_on = [ 
+    aws_api_gateway_integration.form_integration,
+    aws_api_gateway_integration.cors_integration
+   ]
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+  
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  rest_api_id = aws_api_gateway_rest_api.form_api.id
+  deployment_id = aws_api_gateway_deployment.form_api_deployment.id
+  stage_name = "prod"
+}
+
+resource "aws_lambda_permission" "aws_gateway_invoke" {
+  statement_id = "AllowAPIGatewayInvoke"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.form_handler.function_name
+  principal = "apigateway.amazonaws.com"
+  source_arn = "${aws_api_gateway_rest_api.form_api.execution_arn}/*/*"
 }
